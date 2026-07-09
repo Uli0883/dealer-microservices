8c9e5529017a027333c09faa86d7549d3b5db312
Ulises Carrillo <156877334+Uli0883@users.noreply.github.com>
2026-07-09 19:18:25 +0000

    Cambio en JS para mejoramiento

--- dealer-prices/server.js
+++ dealer-prices/server.js
@@ -4,8 +4,12 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 const cors = require('cors');
-app.use(cors());
-
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
 const prices = {
     "1": [
         { dealer: "Dealer A", price: 1000 },
